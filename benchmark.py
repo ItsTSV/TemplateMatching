Fixed SSD calculation and Numba implementation; simplified test, added 4 test cases that will be run later.

benchmark.py
@@ -4,7 +4,7 @@ from tm_algorithms import match_non_parallel, match_parallel
 from tm_algorithms_numba import match_numba_parallel
 
 
-def benchmark_linear(source, template, iterations):
+def benchmark(source, template, iterations, method, process_count=1):
     # List to store times and match map (only one needed, the results will be same for every iteration)
     times = []
     match_map = np.ones_like(source, dtype=np.float32)
@@ -12,41 +12,17 @@ def benchmark_linear(source, template, iterations):
     for i in range(iterations):
         # Start timer, run the algorithm and stop timer
         start = time.time()
-        match_map = match_non_parallel(source, template)
+        if method == "non_parallel":
+            match_map = match_non_parallel(source, template)
+        elif method == "parallel":
+            match_map = match_parallel(source, template, process_count)
+        elif method == "numba":
+            match_map = match_numba_parallel(source, template)
+        else:
+            raise ValueError("Invalid method")
         end = time.time()
         times.append(end - start)
 
     # Return mean time over all iterations and resulting match map
     return np.mean(times), match_map
 
-
-def benchmark_parallel(source, template, iterations, process_count):
-    # List to store times and match map (only one needed, the results will be same for every iteration)
-    times = []
-    match_map = np.ones_like(source, dtype=np.float32)
-
-    for i in range(iterations):
-        # Start timer, run the algorithm and stop timer
-        start = time.time()
-        match_map = match_parallel(source, template, process_count)
-        end = time.time()
-        times.append(end - start)
-
-    # Return mean time over all iterations and resulting match map
-    return np.mean(times), match_map
-
-
-def benchmark_numba(source, template, iterations):
-    # List to store times and match map (only one needed, the results will be same for every iteration)
-    times = []
-    match_map = np.ones_like(source, dtype=np.float32)
-
-    for i in range(iterations):
-        # Start timer, run the algorithm and stop timer
-        start = time.time()
-        match_map = match_numba_parallel(source, template)
-        end = time.time()
-        times.append(end - start)
-
-    # Return mean time over all iterations and resulting match map
-    return np.mean(times), match_map

tm_algorithms.py
@@ -85,12 +85,14 @@ def extract_matches(match_map, threshold):
 
 
 def calculate_score(region, template):
-    # Calculate the SSD using numpy
+    # Normalize images
+    region = region.astype(np.float32) / 255.0
+    template = template.astype(np.float32) / 255.0
+
+    # Calculate SSD
     squared_difference = np.square(region - template)
     ssd = np.sum(squared_difference)
 
-    # Normalize the SSD
+    # Normalize result
     normalized_factor = np.prod(template.shape)
-    normalized_ssd = ssd / normalized_factor
-
-    return normalized_ssd
+    return ssd / normalized_factor

tm_algorithms_numba.py
@@ -2,18 +2,7 @@ import numba
 import numpy as np
 
 
-@numba.jit(nopython=True, parallel=True)
-def parallel_inner_loop(source, template, match_map, start_row, end_row):
-    source_height, source_width = source.shape
-    template_height, template_width = template.shape
-
-    for y in range(start_row, end_row):
-        for x in range(source_width - template_width + 1):
-            region = source[y:y + template_height, x:x + template_width]
-            current_score = calculate_score(region, template)
-            match_map[y, x] = current_score
-
-
+# Mostly non-numeric code, no need to use Numba here
 def match_numba_parallel(source: np.ndarray, template: np.ndarray, start_row=None, end_row=None):
     # Get dimensions of source and template
     source_height, source_width = source.shape
@@ -34,21 +23,39 @@ def match_numba_parallel(source: np.ndarray, template: np.ndarray, start_row=Non
     return match_map
 
 
+# Running with nopython and parallel flag; but only the outer loop can be parallelized effectively
+@numba.jit(nopython=True, parallel=True)
+def parallel_inner_loop(source, template, match_map, start_row, end_row):
+    source_height, source_width = source.shape
+    template_height, template_width = template.shape
+
+    for y in numba.prange(start_row, end_row):
+        for x in range(source_width - template_width + 1):
+            region = source[y:y + template_height, x:x + template_width]
+            current_score = calculate_score(region, template)
+            match_map[y, x] = current_score
+
+
 @numba.jit(nopython=True)
 def calculate_score(region, template):
+    # Normalize images
+    region = region.astype(np.float32) / 255.0
+    template = template.astype(np.float32) / 255.0
+
+    # Calculate SSD
     squared_difference = np.square(region - template)
     ssd = np.sum(squared_difference)
 
+    # Normalize result
     normalized_factor = numba_prod(template.shape)
-    normalized_ssd = ssd / normalized_factor
-
-    return normalized_ssd
+    return ssd / normalized_factor
 
 
-# The np.prod function is not supported by Numba, so we need to define our own
+# Numba -- for some reason -- throws error while using np.prod, so this is my own implementation
 @numba.jit(nopython=True)
 def numba_prod(arr):
     result = 1
     for element in arr:
         result *= element
+
     return result